8cd50c4057ae76c8cc7b9b59a26076394fd74b43
SwayGatsby <kayla.fowler@hotmail.com>
2017-10-30 21:43:43 +0000

    added empty Customer class with to-do list to implement additional connections and methods to Library & Book classes

---
+++ Customer.java
@@ -0,0 +1,6 @@
+//Customers can register with the library
+// TODO add array of customers to Library class
+//Customers can checkout books
+// TODO add checked-out property (type boolean) to Book class
+// TODO add checkout method to Library class to pass in book title & change checked-out property.
+// TODO add isCheckedOut method to Library class to return all checked-out books.
